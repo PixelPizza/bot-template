56f4d518d6e1dd6f6b314b49aaff5e6e0b386c37
Jaron <jaron_zijlstra@outlook.com>
2022-05-04 08:55:21 +0000

    fix: update index without dotenv-parse-variables

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import "@sapphire/plugin-logger/register";
 import { config } from "dotenv";
-import parseEnv from "dotenv-parse-variables";
 import { ApplicationCommandRegistries, RegisterBehavior, SapphireClient } from "@sapphire/framework";
-process.env = parseEnv(config().parsed!) as NodeJS.ProcessEnv;
+config();
 
 async function main() {
 	const client = new SapphireClient({ intents: [] });
