5dcbc24cf620f1be9d6739e2e18c268bbc9cfbf7
Jaron <jaron_zijlstra@outlook.com>
2022-06-26 16:51:20 +0200

    feat: remove files

--- src/augment.d.ts
+++ src/augment.d.ts
@@ -1,14 +1,7 @@
-export declare module "@kaname-png/plugin-env" {
-	export interface EnvKeys {
-		// Add env variables here
-		TOKEN: never;
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			// Add your own environment variables here.
+		}
 	}
-}
-
-export declare module "@sapphire/pieces" {
-	export interface StoreRegistryEntries {
-		// Add stores here
-	}
-
-	export interface Container {}
 }
